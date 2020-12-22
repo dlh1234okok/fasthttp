8e0f489b0c0b706b24cbc23a5acbc8a89bc3fbfa
dulihong <1355591431@qq.com>
2020-12-22 13:29:16 +0800

    realname

---
+++ src/main/java/com/mani/fasthttp/ext/FastHttpConfigException.java
@@ -0,0 +1,15 @@
+package com.mani.fasthttp.ext;
+
+/**
+ * @author Dulihong
+ * @since 2020-12-10
+ */
+public class FastHttpConfigException extends RuntimeException{
+
+    public FastHttpConfigException() {
+    }
+
+    public FastHttpConfigException(String message) {
+        super(message);
+    }
+}
